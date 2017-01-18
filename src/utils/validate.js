29277a3410d2f3e6e3d5b70cbe00578607d08348
Savva <cabehok@inbox.ru>
2017-01-18 16:32:26 +0300

    change alert component. added isClose flag

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,12 +3,19 @@ export function validateEmail(email) {
     return re.test(email);
 }
 
+
+/*(/^
+(?=.*\d)                //хотя бы 1 число
+(?=.*[a-z])             //одна маленькая буква
+(?=.*[A-Z])             //одна заглавная буква
+[a-zA-Z0-9]{8,}         //минимум 8 символов
+$/)*/
 export function validatePassword(password){
 	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
 	return re.test(password);
 }
 
-export function validateOnlyCirilicSymbols(str){
-	var re = /^[a-zA-Z]+$/;
+export function validateLogin(str){
+	var re = /^[a-zA-Z0-9]+$/;
 	return re.test(str);
 }
